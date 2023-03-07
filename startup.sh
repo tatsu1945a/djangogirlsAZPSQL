e90992db4801dec6b41abf99c7dff391198bd3c4
tatsuhiroakita <vmak8160@gmail.com>
2023-03-07 16:35:15 +0900

    17th commit

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ service ssh start
 
 date > env-now.txt
 env >> env-now.txt
-
+cat env-now.txt > /usr/local/app/.env
 python manage.py collectstatic
 python manage.py migrate
 gunicorn config.asgi:application -w 2 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
